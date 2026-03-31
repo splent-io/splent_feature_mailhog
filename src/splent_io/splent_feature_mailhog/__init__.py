28ef6a5e17156ec0587f481ea1e9fc4eeb4f2c9b
drorganvidez <davromorg@alum.us.es>
2026-03-31 00:51:00 +0000

    chore: release splent_io/splent_feature_mailhog to v1.0.4

--- src/splent_io/splent_feature_mailhog/__init__.py
+++ src/splent_io/splent_feature_mailhog/__init__.py
@@ -1,8 +1,6 @@
 from splent_framework.blueprints.base_blueprint import BaseBlueprint
 
-mailhog_bp = BaseBlueprint(
-    "mailhog", __name__, template_folder="templates"
-)
+mailhog_bp = BaseBlueprint("mailhog", __name__, template_folder="templates")
 
 
 def init_feature(app):
--- src/splent_io/splent_feature_mailhog/config.py
+++ src/splent_io/splent_feature_mailhog/config.py
@@ -10,12 +10,14 @@ import os
 
 
 def inject_config(app):
-    app.config.update({
-        "MAIL_SERVER": os.getenv("MAILHOG_HOST", "splent_feature_mailhog"),
-        "MAIL_PORT": int(os.getenv("MAILHOG_SMTP_PORT", "1025")),
-        "MAIL_USE_TLS": False,
-        "MAIL_USE_SSL": False,
-        "MAIL_USERNAME": "",
-        "MAIL_PASSWORD": "",
-        "MAIL_DEFAULT_SENDER": os.getenv("MAIL_DEFAULT_SENDER", "dev@splent.local"),
-    })
+    app.config.update(
+        {
+            "MAIL_SERVER": os.getenv("MAILHOG_HOST", "splent_feature_mailhog"),
+            "MAIL_PORT": int(os.getenv("MAILHOG_SMTP_PORT", "1025")),
+            "MAIL_USE_TLS": False,
+            "MAIL_USE_SSL": False,
+            "MAIL_USERNAME": "",
+            "MAIL_PASSWORD": "",
+            "MAIL_DEFAULT_SENDER": os.getenv("MAIL_DEFAULT_SENDER", "dev@splent.local"),
+        }
+    )
